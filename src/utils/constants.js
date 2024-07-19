c1e5c418faa44bb79573cb6b3265943205fe0d8f
Vishal Yadav <102068244+Vishalydv07@users.noreply.github.com>
2024-07-20 04:06:44 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://ibb.co/j57rTby';
+
+export const categories = [
+  { name: 'All', icon: <HomeIcon /> },
+  { name: 'Neetcode', icon: <CodeIcon /> },
+  { name: 'Back To Back SWE', icon: <CodeIcon /> },
+  { name: 'Abdul Bari', icon: <CodeIcon /> },
+  { name: 'Tushar Roy - Coding Made Simple', icon: <CodeIcon /> },
+  { name: 'mycodeschool', icon: <SchoolIcon /> },
+  { name: 'Algorithms with Attitude', icon: <CodeIcon /> },
+  { name: 'GeeksforGeeks', icon: <CodeIcon /> },
+  { name: 'Jenny\'s Lectures CS IT', icon: <SchoolIcon /> },
+  { name: 'Errichto', icon: <CodeIcon /> },
+  { name: 'William Fiset', icon: <CodeIcon /> },
+  { name: 'Kevin Naughton Jr.', icon: <CodeIcon /> },
+  { name: 'Nick White', icon: <CodeIcon /> },
+  { name: 'Gaurav Sen', icon: <CodeIcon /> },
+  { name: 'Clément Mihailescu', icon: <CodeIcon /> },
+  { name: 'Reducible', icon: <CodeIcon /> },
+  { name: 'CS Dojo', icon: <SchoolIcon /> },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
